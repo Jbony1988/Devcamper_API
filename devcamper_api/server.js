594745b3aaac1050b407bf93807c92b734a376f6
jbony88 <info@platowdesigns.com>
2019-11-08 21:00:04 -0600

    added crud function to bootcamp collection

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
+
+const connectDB = require("./config/db");
+
+// load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// connect to database
+connectDB();
 
 // Route files
 
 const bootcamps = require("./routes/bootcamp");
-dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Dev logging middle ware
 if (process.env.NODE_ENV === "development") {
@@ -20,8 +31,17 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} 
-  mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} 
+  mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // close server and exit process
+  server.close(() => process.exit(1));
+});
