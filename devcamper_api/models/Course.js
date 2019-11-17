11c6e8957cc9253382e0bff47b25179a859a2761
jbony88 <info@platowdesigns.com>
2019-11-17 16:29:55 -0600

    added delete bootcamp with courses functionality

--- devcamper_api/models/Course.js
+++ devcamper_api/models/Course.js
@@ -1,42 +1,50 @@
 const mongoose = require("mongoose");
 
-const CourseSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    trim: true,
-    required: [true, "Please add  course title"]
-  },
-  description: {
-    type: String,
-    required: [true, "Please add a description"]
-  },
+const CourseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "Please add  course title"]
+    },
+    description: {
+      type: String,
+      required: [true, "Please add a description"]
+    },
 
-  weeks: {
-    type: String,
-    required: [true, "Please add number of weeks"]
+    weeks: {
+      type: String,
+      required: [true, "Please add number of weeks"]
+    },
+    tuition: {
+      type: Number,
+      required: [true, "Please add a tuition cost"]
+    },
+    minimumSkill: {
+      type: String,
+      required: [true, "Please add a minimum skill"],
+      enum: ["beginner", "intermediate", "advance"]
+    },
+    scholarShipsAvailable: {
+      type: Boolean,
+      default: false
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    bootcamp: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Bootcamp",
+      required: true
+    }
   },
-  tuition: {
-    type: Number,
-    required: [true, "Please add a tuition cost"]
-  },
-  minimumSkill: {
-    type: String,
-    required: [true, "Please add a minimum skill"],
-    enum: ["beginner", "intermediate", "advance"]
-  },
-  scholarShipsAvailable: {
-    type: Boolean,
-    default: false
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  bootcamp: {
-    type: mongoose.Schema.ObjectId,
-    ref: "Bootcamp",
-    required: true
+  {
+    toJSON: {
+      virtuals: true
+    },
+    toObject: { virtuals: true }
   }
-});
+);
 
 module.exports = mongoose.model("Course", CourseSchema);
